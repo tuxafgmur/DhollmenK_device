4e5fc554ef9ceabd4cbae64444494896674b8cbc
tuxafgmur <usuario@localhost.localdomain>
2014-09-27 20:42:40 +0200

    Initial release
    Copy of  https://github.com/SlimRoms/device_samsung    - kk4.4 - 2014.09.27
    (p3100 p3110 p5100 p5110 omap4-common)
     https://android.googlesource.com/device/common          2014.09.27
     https://android.googlesource.com/device/generic/common  2014.09.27

---
+++ samsung/p3110/slim.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2014 SlimRoms Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := p3110
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1024
+TARGET_SCREEN_WIDTH := 600
+
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_tablet_wifionly.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/p3110/full_p3110.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := p3110
+PRODUCT_NAME := slim_p3110
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GT-P3110
+PRODUCT_MANUFACTURER := samsung
+
+#Set build fingerprint / ID / Prduct Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=espressowifixx \
+    TARGET_DEVICE=espressowifi \
+    BUILD_FINGERPRINT="samsung/espressowifixx/espressowifi:4.2.2/JDQ39/P3110XXDMH1:user/release-keys" \
+    PRIVATE_BUILD_DESC="espressowifixx-user 4.2.2 JDQ39 P3110XXDMH1 release-keys"
