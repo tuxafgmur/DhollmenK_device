b7b2868073be1a62115785ddc160bb1d827754f0
tuxafgmur <usuario@localhost.localdomain>
2015-02-17 02:37:39 +0100

    Unifies device resources - common-overlay

--- samsung/p3100/p31xx-common.mk
+++ samsung/p3100/p31xx-common.mk
@@ -15,14 +15,16 @@
 # Include omap4 common makefile
 $(call inherit-product, device/samsung/omap4-common/common.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/samsung/p3100/common-overlay
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 LOCAL_PATH := device/samsung
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/p3100/common-overlay \
+    $(LOCAL_PATH)/tab2-common/common-overlay
 
 PRODUCT_CHARACTERISTICS := tablet
 
